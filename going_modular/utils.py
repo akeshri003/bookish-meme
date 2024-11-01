d8de5cefc59a29da526d5cb20f884e445641c56e
akeshri003 <101141783+akeshri003@users.noreply.github.com>
2024-11-01 14:46:22 +0530

    updated utils.py

--- going_modular/utils.py
+++ going_modular/utils.py
@@ -1,9 +1,15 @@
 """
 Contains various utility functions for PyTorch model training and saving.
 """
+import matplotlib.pyplot as plt
+
 from pathlib import Path
+from torchvision import transforms, datasets
+from typing import Dict, Tuple
+from PIL import Image
 
 import torch
+import torchvision
 
 def save_model(model: torch.nn.Module,
                target_dir: str,
@@ -34,3 +40,70 @@ def save_model(model: torch.nn.Module,
   print(f"[INFO] Saving the model to {model_save_path}")
   torch.save(obj=model.state_dict(),
              f=model_save_path)
+
+
+def plot_loss_curves(results: Dict):
+  """Plots a graph of train and test loss and accuracy over the epochs, during training.
+  """
+  loss = results["train_loss"]
+  test_loss = results["test_loss"]
+
+  acc = results["train_acc"]
+  test_acc = results["test_acc"]
+
+  epochs = range(len(results["train_loss"]))
+
+  plt.figure(figsize=(15, 7))
+  plt.subplot(1, 2, 1)
+  plt.plot(epochs, loss, label="Train Loss")
+  plt.plot(epochs, test_loss, label="Test loss")
+  plt.title("Loss Curves")
+  plt.xlabel("Epochs")
+  plt.legend()
+
+  plt.subplot(1,2,2)
+  plt.plot(epochs, acc, label="Train accuracy")
+  plt.plot(epochs, test_acc, label="Test accuracy")
+  plt.title("Accuracy")
+  plt.xlabel("Epochs")
+  plt.legend()
+
+def pred_plot_image(model: torch.nn.Module, 
+                    image_path: str,
+                    class_names: List[str],
+                    image_size: Tuple[int, int]= (224, 224),
+                    transform: torchvision.transforms = None,
+                    device: torch.device="cpu"
+                    ):
+  
+  img = Image.open(image_path)
+
+  if transform is not None:
+    image_transform = transform
+  else:
+    image_transform = transforms.Compose([
+      transforms.Resize(size=image_size),
+      transforms.ToTensor(),
+      transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+    ])
+
+  transformed_img = image_transform(img)
+
+  model.to(device)
+
+  model.eval()
+  with torch.inference_mode():
+    transformed_img = image_transform(img).unsqueeze(dim=0)
+    logits = model(transformed_img.to(device))
+    pred_probs = torch.softmax(logits, dim=1)
+    pred_prob_class = torch.argmax(pred_probs, dim=1)
+
+  plt.figure()
+  plt.imshow(img)
+  plt.title(f"Pred: {class_names[target_image_pred_label]} | Prob: {target_image_pred_probs.max():.3f}")
+  plt.axis(False)
+
+
+
+
